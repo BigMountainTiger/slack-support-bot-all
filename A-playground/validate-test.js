9314d6b31c25334ab0249057e827e517392199a8
Song Li <da_tou_li@yahoo.com>
2020-04-14 15:35:31 -0400

    Played with the validation of slack

--- A-playground/validate-test.js
+++ A-playground/validate-test.js
@@ -1,14 +1,40 @@
 require('dotenv').config();
+const axios = require('axios');
+const CancelToken = axios.CancelToken;
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+const getCancelToken = () => {
+  let source = CancelToken.source();
+  setTimeout(() => { source.cancel('Timeout'); }, 3 * 1000);
+
+  return source.token;
+};
+
+const validate_slack = async () => {
+  const url = 'https://slack.com1/api/auth.test';
+
+  const options = {
+    method: 'GET',
+    cancelToken: getCancelToken(),
+    headers: { Authorization: 'Bearer ' + BOT_TOKEN },
+    url: url,
+  };
+
+  try {
+    const res = await axios(options);
+    return res.data
+  } catch(e) {
+    return e;
+  }
+};
 
 (async () => {
-  const p = new Promise((rs, rj) => {
-    setTimeout(() => {
-      rs('OK');
-    }, 1000);
-  });
+  let slack_data = await validate_slack();
 
-  const r = await p;
-  console.log(r);
+  console.log({
+    slack_data: slack_data
+  });
 })();
 
 console.log('Initiated ...');
