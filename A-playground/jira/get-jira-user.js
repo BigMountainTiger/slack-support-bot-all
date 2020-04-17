2c4ab5b9871038f96cec2c7bffb6397832d58d19
Song Li <da_tou_li@yahoo.com>
2020-04-17 16:02:01 -0400

    Added the clearing of the timeout

--- A-playground/jira/get-jira-user.js
+++ A-playground/jira/get-jira-user.js
@@ -8,24 +8,29 @@ const CancelToken = axios.CancelToken;
 const JIRA_AUTH_EMAIL = process.env.JIRA_AUTH_EMAIL;
 const JIRA_AUTH_TOKEN = process.env.JIRA_AUTH_TOKEN;
 
-const getCancelToken = (seconds) => {
-  let source = CancelToken.source();
-  setTimeout(() => { source.cancel('Timeout'); }, seconds * 1000);
+// const getCancelToken = (seconds) => {
+//   let source = CancelToken.source();
+//   setTimeout(() => { source.cancel('Timeout'); }, seconds * 1000);
 
-  return source.token;
-};
+//   return source.token;
+// };
 
 const get_jira_user = async (email) => {
   const url = `https://mlg-playground.atlassian.net/rest/api/2/user/search?query=${escape(email)}`;
 
+  const source = CancelToken.source();
+  const timeoutHandle = setTimeout(() => { source.cancel('Timeout'); }, 10 * 1000);
+
   const options = {
     method: 'GET',
-    cancelToken: getCancelToken(3),
+    cancelToken: source.token,
     auth: { username: JIRA_AUTH_EMAIL, password: JIRA_AUTH_TOKEN },
     url: url,
   };
 
   const res = await axios(options);
+  clearTimeout(timeoutHandle);
+
   return res.data;
 };
 
