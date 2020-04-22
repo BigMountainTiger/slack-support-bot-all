fe337a2a8d237efe625fee69fa31e9320893cd57
Song Li <da_tou_li@yahoo.com>
2020-04-21 20:55:53 -0400

    Played with getting jira users by email

--- A-playground/jira/get-jira-user.js
+++ A-playground/jira/get-jira-user.js
@@ -1,3 +1,12 @@
+// https://community.atlassian.com/t5/Jira-Service-Desk-questions/API-responses-suddenly-missing-emailAddress-field/qaq-p/1125871
+
+// The missing field(s) are by design; it's not a bug but a choice made to comply with GDPR.
+// Individual users can choose to disclose their email through the API but this means some will and some wont.
+// A new API will be added in the future where you can get a list of all e-mails,
+// however this requires whitelisting of your application.
+// If this change breaks your code, then the only solution is to change the logic OR use the new API (when its available)
+// OR have all your users disclose their email. For more information, see the links in the official answer below.
+
 // https://community.atlassian.com/t5/Answers-Developer-Questions/Create-New-Users-using-JIRA-REST-API/qaq-p/541525
 
 require('dotenv').config({ path: __dirname + '/../.env' });
@@ -9,6 +18,7 @@ const JIRA_AUTH_TOKEN = process.env.JIRA_AUTH_TOKEN;
 
 const get_jira_user = async (email) => {
   const url = `https://mlg-playground.atlassian.net/rest/api/2/user/search?query=${escape(email)}`;
+  //const url = `https://mlg-playground.atlassian.net/rest/api/2/user/search?query=${email}`;
 
   const options = {
     method: 'GET',
@@ -16,14 +26,22 @@ const get_jira_user = async (email) => {
     url: url,
   };
 
-  let res = await ext_axios(options);
+  let res; 
+  try {
+    res = await ext_axios(options);
+  } catch(e) {
+    console.log(e.response.data);
+  }
+  
 
   return res.data;
 };
 
 (async () => {
 
-  const data = await get_jira_user('song-004@monsterlg.com');
+  const data = await get_jira_user('song@monsterlg.ld.sp.jp.com');
+
+  //const data = await get_jira_user('song@monsterlg.com');
   //const data = await get_jira_user('andre@monsterlg.com');
   //const data = await get_jira_user('ashley@monsterlg.com');
   
